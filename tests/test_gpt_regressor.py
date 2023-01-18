5a1146bae0bafb5b89fa13a0b653af3aac45e1f4
Kevin Maik Jablonka <kevin.jablonka@epfl.ch>
2023-01-18 09:03:19 +0100

    chore: update

---
+++ tests/test_gpt_regressor.py
@@ -0,0 +1,11 @@
+from gptchem.gpt_regressor import GPTRegressor
+
+from gptchem.data import get_photoswitch_data
+
+
+def test_gpt_regression():
+    """Test GPT-3 regression."""
+    data = get_photoswitch_data()
+    gpt = GPTRegressor("pce", tuner=None)
+    data = gpt._prepare_df(data["SMILES"], data["E isomer pi-pi* wavelength in nm"])
+    assert data.shape == (403, 2)
